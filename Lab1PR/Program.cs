867c93687b91d9243febbb5761c6c249f0606c4e
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2024-02-15 14:52:10 +0300

    Добавьте файлы проекта.

---
+++ Lab1PR/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите X: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Результат: " + CalcSum(n, x));
+    }
+
+    static double CalcSum(int n, int x)
+    {
+        double sum = 0;
+        for (int nn = 0; nn <= n; nn++)
+        {
+            sum += 1 / ((2 * nn + 1) * Math.Pow(x, 2 * nn + 1));
+        }
+        return sum;
+    }
+}
